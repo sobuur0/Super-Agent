1a19673b2af80617a42cdef62eb03cab83ea5895
t-bolt <isholasobuur@gmail.com>
2020-12-08 20:44:36 +0100

    login ui

--- app/src/main/java/com/example/superagent/MainActivity.java
+++ app/src/main/java/com/example/superagent/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.superagent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.superagent.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.hello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivty.class);
+                startActivity(intent);
+            }
+        });
     }
 }
