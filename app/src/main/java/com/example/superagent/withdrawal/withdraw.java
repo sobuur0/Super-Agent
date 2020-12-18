f086eeefd796567216806588f9307a5793c34b9c
t-bolt <isholasobuur@gmail.com>
2020-12-18 16:32:07 +0100

    binded withdraw view to its controller

---
+++ app/src/main/java/com/example/superagent/withdrawal/withdraw.java
@@ -0,0 +1,18 @@
+package com.example.superagent.withdrawal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.superagent.databinding.ActivityWithdrawBinding;
+
+public class withdraw extends AppCompatActivity {
+    private ActivityWithdrawBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityWithdrawBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
