4a8a414a92a9810e33e21ebee506900ca35c6a5a
t-bolt <isholasobuur@gmail.com>
2020-12-24 22:42:27 +0100

    added the backArrow button to withdrawSuccess screen

--- app/src/main/java/com/example/superagent/withdrawal/WithdrawSuccess.java
+++ app/src/main/java/com/example/superagent/withdrawal/WithdrawSuccess.java
@@ -1,19 +1,36 @@
 package com.example.superagent.withdrawal;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 
+import com.example.superagent.R;
 import com.example.superagent.databinding.ActivityWithdrawSuccessBinding;
+
+import java.util.Objects;
 
 public class WithdrawSuccess extends AppCompatActivity {
 
     private ActivityWithdrawSuccessBinding binding;
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityWithdrawSuccessBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Drawable backArrow = ContextCompat.getDrawable(this, R.drawable.arrowback);
+
+        setSupportActionBar(binding.toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(backArrow);
+        getSupportActionBar().setTitle("");
+        binding.appBarLayout.bringToFront();
     }
 }
