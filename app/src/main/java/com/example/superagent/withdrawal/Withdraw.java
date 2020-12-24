4890bfe61c54631e5b5690550a7445d834a5763c
t-bolt <isholasobuur@gmail.com>
2020-12-24 22:40:21 +0100

    added the backArrow button to withdrawal screen

--- app/src/main/java/com/example/superagent/withdrawal/Withdraw.java
+++ app/src/main/java/com/example/superagent/withdrawal/Withdraw.java
@@ -1,21 +1,41 @@
 package com.example.superagent.withdrawal;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 
+import com.example.superagent.R;
 import com.example.superagent.databinding.ActivityWithdrawBinding;
+
+import java.util.Objects;
 
 public class Withdraw extends AppCompatActivity {
     private ActivityWithdrawBinding binding;
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityWithdrawBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Drawable backArrow = ContextCompat.getDrawable(this, R.drawable.arrowback);
+
+        setSupportActionBar(binding.toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(backArrow);
+        getSupportActionBar().setTitle("");
+        binding.appBarLayout.bringToFront();
 
         binding.btnWithdraw.setOnClickListener(new View.OnClickListener() {
             @Override
