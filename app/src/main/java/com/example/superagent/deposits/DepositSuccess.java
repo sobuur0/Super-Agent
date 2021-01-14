dfca3bf7f8531e2a6570997db5756cf54b6e7283
t-bolt <isholasobuur@gmail.com>
2021-01-14 15:01:10 +0100

    added a backArrow actionbar

--- app/src/main/java/com/example/superagent/deposits/DepositSuccess.java
+++ app/src/main/java/com/example/superagent/deposits/DepositSuccess.java
@@ -1,22 +1,39 @@
 package com.example.superagent.deposits;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.superagent.R;
 import com.example.superagent.databinding.ActivityDepositSuccessBinding;
 import com.example.superagent.home.HomeScreen;
+
+import java.util.Objects;
 
 public class DepositSuccess extends AppCompatActivity {
     private ActivityDepositSuccessBinding binding;
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityDepositSuccessBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Drawable backArrow = ContextCompat.getDrawable(this, R.drawable.arrowback);
+
+        setSupportActionBar(binding.toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(backArrow);
+        getSupportActionBar().setTitle("");
+        binding.appBarLayout.bringToFront();
 
         binding.btnBackHome.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/superagent/deposits/DepositTransaction.java
+++ app/src/main/java/com/example/superagent/deposits/DepositTransaction.java
@@ -1,22 +1,38 @@
 package com.example.superagent.deposits;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.superagent.R;
 import com.example.superagent.databinding.ActivityDepositTransactionBinding;
+
+import java.util.Objects;
 
 public class DepositTransaction extends AppCompatActivity {
     private ActivityDepositTransactionBinding binding;
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityDepositTransactionBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        Drawable backArrow = ContextCompat.getDrawable(this, R.drawable.arrowback);
+
+        setSupportActionBar(binding.toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(backArrow);
+        getSupportActionBar().setTitle("");
+        binding.appBarLayout.bringToFront();
 
         binding.btnDeposit.setOnClickListener(new View.OnClickListener() {
             @Override
