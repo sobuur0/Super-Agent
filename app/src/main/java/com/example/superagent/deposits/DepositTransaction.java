b526d464d44ba54ec32ff178734f916c8351a1ee
t-bolt <isholasobuur@gmail.com>
2021-01-14 14:00:04 +0100

    set an intent from deposit transaction to deposit success screen

--- app/src/main/java/com/example/superagent/deposits/DepositTransaction.java
+++ app/src/main/java/com/example/superagent/deposits/DepositTransaction.java
@@ -2,7 +2,9 @@ package com.example.superagent.deposits;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.superagent.databinding.ActivityDepositTransactionBinding;
 
@@ -14,5 +16,14 @@ public class DepositTransaction extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityDepositTransactionBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+
+        binding.btnDeposit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), DepositSuccess.class);
+                startActivity(intent);
+            }
+        });
     }
 }
