fe1a127ac55583a19587c1e9acb8b7f16c0efc2f
t-bolt <isholasobuur@gmail.com>
2021-01-14 13:14:39 +0100

    full ui of DepositTransaction

---
+++ app/src/main/java/com/example/superagent/deposits/DepositTransaction.java
@@ -0,0 +1,14 @@
+package com.example.superagent.deposits;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DepositTransaction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
