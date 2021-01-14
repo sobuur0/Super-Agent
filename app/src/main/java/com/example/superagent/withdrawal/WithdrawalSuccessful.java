8c5bfc477b95db45d095e859c2adb3fcb90f0802
t-bolt <isholasobuur@gmail.com>
2021-01-14 16:15:37 +0100

    full ui for withdrawal successful

---
+++ app/src/main/java/com/example/superagent/withdrawal/WithdrawalSuccessful.java
@@ -0,0 +1,46 @@
+package com.example.superagent.withdrawal;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.superagent.R;
+import com.example.superagent.databinding.ActivityWithdrawalSuccessfulBinding;
+import com.example.superagent.home.HomeScreen;
+
+import java.util.Objects;
+
+public class WithdrawalSuccessful extends AppCompatActivity {
+    private ActivityWithdrawalSuccessfulBinding binding;
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityWithdrawalSuccessfulBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Drawable backArrow = ContextCompat.getDrawable(this, R.drawable.arrowback);
+
+        setSupportActionBar(binding.toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(backArrow);
+        getSupportActionBar().setTitle("");
+        binding.appBarLayout.bringToFront();
+
+        binding.btnBackHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), HomeScreen.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
