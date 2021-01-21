ba6c0106e30288aae39ae27a9ba5178f2fb45273
t-bolt <isholasobuur@gmail.com>
2021-01-21 21:39:00 +0100

    refactored the login screen from an activity to fragment

--- app/src/main/java/com/example/superagent/loginSignUp/LoginActivity.java
+++ app/src/main/java/com/example/superagent/loginSignUp/LoginActivity.java
@@ -2,6 +2,8 @@ package com.example.superagent.loginSignUp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +11,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.superagent.R;
 import com.example.superagent.databinding.ActivityLoginActivtyBinding;
 
 import java.util.HashMap;
@@ -25,13 +28,14 @@ public class LoginActivity extends AppCompatActivity {
         binding = ActivityLoginActivtyBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, SetPin.class);
-                startActivity(intent);
-            }
-        });
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment =new  LoginFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 
 
---
+++ app/src/main/java/com/example/superagent/loginSignUp/LoginFragment.java
@@ -0,0 +1,41 @@
+package com.example.superagent.loginSignUp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.superagent.R;
+
+public class LoginFragment extends Fragment {
+    private Button btnLogin;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+
+        btnLogin = v.findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), SetPin.class);
+                startActivity(intent);
+            }
+        });
+
+        return v;
+    }
+
+}
