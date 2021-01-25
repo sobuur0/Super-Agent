40df440a2a58e999b9cb3d484db9be86c9fe9c1f
t-bolt <isholasobuur@gmail.com>
2021-01-25 02:05:16 +0100

    wiring up TransactionsHistoryFragment with its activity

--- app/src/main/java/com/example/superagent/transactions/TransactionsHistory.java
+++ app/src/main/java/com/example/superagent/transactions/TransactionsHistory.java
@@ -3,6 +3,8 @@ package com.example.superagent.transactions;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.graphics.drawable.Drawable;
 import android.os.Build;
@@ -23,7 +25,14 @@ public class TransactionsHistory extends AppCompatActivity {
         binding = ActivityTransactionsHistoryBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_transactions_history);
+        if (fragment == null) {
+            fragment = new TransactionFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_transactions_history, fragment)
+                    .commit();
+        }
 
 
 
