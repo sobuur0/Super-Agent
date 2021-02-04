c408ad39569db790651d69e5f7cc5f876dba566e
t-bolt <isholasobuur@gmail.com>
2021-02-04 03:41:03 +0100

    added the fragment java class for the signup

---
+++ app/src/main/java/com/example/superagent/loginSignUp/SignUpFragment.java
@@ -0,0 +1,30 @@
+package com.example.superagent.loginSignUp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.superagent.R;
+
+public class SignUpFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_signup, container, false);
+
+
+
+        return v;
+    }
+}
